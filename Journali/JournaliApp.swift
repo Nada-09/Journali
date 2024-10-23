6eec54ba699f1db122a33663af42e22ef009337d
Nada-09 <nadaalmajidi4@gmail.com>
2024-10-23 08:56:21 +0300

    Initial Commit

---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by Nada Abdullah on 20/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JournaliTests/JournaliTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliTests.swift
+//  JournaliTests
+//
+//  Created by Nada Abdullah on 20/04/1446 AH.
+//
+
+import Testing
+@testable import Journali
+
+struct JournaliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
