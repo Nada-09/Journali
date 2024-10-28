89f05e22464b010a9484e9dbff99c46e9c8d682d
Nada-09 <nadaalmajidi4@gmail.com>
2024-10-28 12:51:10 +0300

    ٠٩

---
+++ Journali/EmptyStateNew.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyStateNew.swift
+//  Journali
+//
+//  Created by Nada Abdullah on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+struct EmptyStateNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyStateNew()
+}
---
+++ Journali/NewJournal.swift
@@ -0,0 +1,18 @@
+//
+//  NewJournal.swift
+//  Journali
+//
+//  Created by Nada Abdullah on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+struct NewJournal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewJournal()
+}
